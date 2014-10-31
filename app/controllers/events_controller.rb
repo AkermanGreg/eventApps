ead456690f6a9c5f481dd2dc42bef592b153f30b
AkermanGreg <gregoryakerman@gmail.com>
2014-10-31 08:25:47 -0700

    Friday October 31

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,7 @@ class EventsController < ApplicationController
 
 	def index
 		@events = Event.all
+
 		
 	end
 
@@ -17,12 +18,13 @@ class EventsController < ApplicationController
 
 	def new
 		@event = Event.new
+
 	end
 
 	def create
-
+		
 	    @event = Event.new(params.require(:event).permit(:date, :time, :title, :address, :city, :state, :zip, :desc))
-	    
+	    @date =  @event["date(1i)"].to_i, @event["date(2i)"].to_i, @event["date(3i)"].to_i
 
 	    if @event.save
 	        redirect_to events_path
@@ -35,11 +37,16 @@ class EventsController < ApplicationController
 
 	def edit
 		@event = Event.find(params[:id])
+
+
 	end
 
    def update
+   	
     @event = Event.find(params[:id])
-	    if @event.update_attributes(params.require(:event).permit(:date, :time, :title, :address, :city, :state, :zip, :desc))
+ 	@date =  @event["date(1i)"].to_i, @event["date(2i)"].to_i, @event["date(3i)"].to_i
+
+	    if @event.update_attributes(params.require(:event).permit(:title, :date, :time, :address, :city, :state, :zip, :desc))
 	        redirect_to events_path
 	     else
 	        render 'edit'
